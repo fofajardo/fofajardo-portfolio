96d1f40a0c76872827b59dcb78280b8a7c45d364
Francis Dominic Fajardo <fajardo@duck.com>
2026-01-09 01:02:22 +0800

    chore: add @sveltejs/enhanced-img

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { sveltekit } from '@sveltejs/kit/vite';
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		enhancedImages(),
+		sveltekit()
+	]
 });
