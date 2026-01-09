e9b2111d847f70817cab94cecb764eb1087ba0b6
Francis Dominic Fajardo <fajardo@duck.com>
2026-01-09 18:40:59 +0800

    refactor: move favicon, PWA, and other scripts to static data.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,4 @@
 <script lang="ts">
-	import faviconIco from "$lib/assets/favicon.ico";
-	import faviconSvg from "$lib/assets/favicon.svg";
-	import favicon96x96Png from "$lib/assets/favicon-96x96.png";
-	import appleTouchIconPng from "$lib/assets/apple-touch-icon.png";
-	import siteWebmanifest from "$lib/assets/site.webmanifest";
 	import "$lib/assets/common.css";
 
 	let { children } = $props();
@@ -22,26 +17,8 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" type="image/png" href={favicon96x96Png} sizes="96x96" />
-	<link rel="icon" type="image/svg+xml" href={faviconSvg} />
-	<link rel="shortcut icon" href={faviconIco} />
-	<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIconPng} />
-	<meta name="apple-mobile-web-app-title" content="fofajardo" />
-	<link rel="manifest" href={siteWebmanifest} />
-	<!-- OpenGraph -->
 	<meta property="og:title" content="Francis Dominic Fajardo - Software Developer">
 	<meta property="og:description" content="Francis Dominic Fajardo's Portfolio">
-	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-CB1CL6K9LN"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag("js", new Date());
-		gtag("config", "G-CB1CL6K9LN");
-	</script>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
-	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </svelte:head>
 
 {@render children()}
