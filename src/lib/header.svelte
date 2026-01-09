f635df01e30f65b2f747a4f0c761daa125f1095c
Francis Dominic Fajardo <fajardo@duck.com>
2026-01-09 18:30:44 +0800

    refactor: improve header structure.

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,36 +1,28 @@
 <script lang="ts">
     import Icon from "@iconify/svelte";
+    import { page } from "$app/state";
     
     let { data } = $props();
 </script>
 
-<header>
-    <a href="/">
-        <section class="bg cl cl1 cl1o">
-            <div class="center">
-                <span class="title">Francis Dominic Fajardo</span>
-                <span class="subtitle">Software Developer</span>
-            </div>
-        </section>
-    </a>
-    <section id="contact">
-        <div class="center">
-            <ul class="group-ab">
-                {#each data.nav as { href, icon, label }}
-                    <li>
-                        <a href={href} class="action-button">
-                            <Icon class="icon" {icon} />
-                            <span class="label visually-hidden">{label}</span>
-                        </a>
-                    </li>
-                {/each}
-            </ul>
-        </div>
-    </section>
+<header class="primary-header cl1o" aria-label="Site header">
+    <div class="center">
+        <span class="title">Francis Dominic Fajardo</span>
+        <span class="subtitle">Software Developer</span>
+    </div>
 </header>
 
-<style>
-    header {
-        view-transition-name: header;
-    }
-</style>
+<nav class="primary-nav" aria-label="Primary navigation">
+    <ul class="group-ab">
+        {#each data.nav as { href, icon, label }}
+            {#if page.url.pathname != href}
+            <li>
+                <a href={href} class="action-button">
+                    <Icon class="icon" {icon} />
+                    <span class="label visually-hidden">{label}</span>
+                </a>
+            </li>
+            {/if}
+        {/each}
+    </ul>
+</nav>
