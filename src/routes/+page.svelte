52cdb7fd70c8b67a5079ab84d9b8ec5a09378d8d
Francis Dominic Fajardo <fajardo@duck.com>
2026-01-09 18:31:46 +0800

    ui: open blog link in new tab.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,7 +21,7 @@
             <div class="card">
                 <div>
                     <p style="text-align: justify; margin: 0;">
-                        I'm a Computer Science student at the <a target="_blank" href="https://uplb.edu.ph/"><Icon icon="tabler:school" /> University of the Philippines Los Baños</a>.<br/><br/> I've been fascinated with technology since I first used our old family computer as a kid. I am also a fan of Filipino writing and poetry, RPGs, strategy and simulation games, and retro gaming. I occasionally post about some of those topics in my blog, <a href="http://saligsig.wordpress.com/"><Icon icon="tabler:brand-wordpress" /> Saligsig</a>.
+                        I'm a Computer Science student at the <a target="_blank" href="https://uplb.edu.ph/"><Icon icon="tabler:school" /> University of the Philippines Los Baños</a>.<br/><br/> I've been fascinated with technology since I first used our old family computer as a kid. I am also a fan of Filipino writing and poetry, RPGs, strategy and simulation games, and retro gaming. I occasionally post about some of those topics in my blog, <a target="_blank" href="https://saligsig.wordpress.com/"><Icon icon="tabler:brand-wordpress" /> Saligsig</a>.
                         <br/><br/>
                         Take a closer look at my <a target="_blank" href="https://drive.google.com/file/d/1FkoQdk_GV_Ad2Lb7VSxhBX6uddumziwo/view?usp=drive_link"><Icon icon="tabler:file-type-pdf" /> résumé</a> to explore my qualifications and experiences.
                     </p>
