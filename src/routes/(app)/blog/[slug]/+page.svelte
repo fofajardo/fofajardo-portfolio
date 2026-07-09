aaab840c675c37c7a0d2cb0cc5a55c2fd86e4a02
Francis Dominic Fajardo <fajardo@duck.com>
2026-07-09 12:12:02 +0800

    fix: missing attribute value for preconnect.

--- src/routes/(app)/blog/[slug]/+page.svelte
+++ src/routes/(app)/blog/[slug]/+page.svelte
@@ -20,7 +20,7 @@
 
 <svelte:head>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
   <link
     href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap"
     rel="stylesheet"
