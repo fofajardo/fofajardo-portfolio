23123c6553fcc4a22317cf1759dea5a4f264bc03
Francis Dominic Fajardo <fajardo@duck.com>
2026-07-10 15:43:17 +0800

    chore: switch to using the vite plugin for svelte-sitemap.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { svelteSitemap } from "svelte-sitemap/vite";
 
 export default defineConfig({
-  plugins: [enhancedImages(), sveltekit()]
+  plugins: [
+    enhancedImages(),
+    sveltekit(),
+    svelteSitemap({
+      domain: "https://fofajardo.is-a.dev",
+      outDir: ".vercel/output/static"
+    })
+  ]
 });
