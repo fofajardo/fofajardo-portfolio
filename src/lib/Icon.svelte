92ea196c63afd1823787ee3cbfebcbdcb259919a
Francis Dominic Fajardo <fajardo@duck.com>
2026-09-17 20:34:25 +0800

    chore: add more icons.

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -128,8 +128,14 @@
   import PhProjectorScreenBold from "~icons/ph/projector-screen-bold";
   import PhBinaryBold from "~icons/ph/binary-bold";
   import PhWindowsLogoFill from "~icons/ph/windows-logo-fill";
+  import DeviconPlainDocker from "~icons/devicon-plain/docker";
+  import DeviconVercel from "~icons/devicon/vercel";
+  import BxlVisualStudio from "~icons/bxl/visual-studio";
 
   const iconMap: Record<string, any> = {
+    "bxl:visual-studio": BxlVisualStudio,
+    "devicon:vercel": DeviconVercel,
+    "devicon-plain:docker": DeviconPlainDocker,
     "line-md:home": LineMdHome,
     "line-md:briefcase": LineMdBriefcase,
     "line-md:construction": LineMdConstruction,
